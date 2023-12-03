de13bc65c8fc8f94d9bda6bca0dd17236e8515ee
Álvaro C <castillaloaizaalvaro@gmail.com>
2023-12-04 00:10:59 +0100

    45% -> Menú opciones para ocultar cartas y lógica hecha

--- app/src/main/java/com/acasloa946/blackjack/userInterface/1vs1ViewModel.kt
+++ app/src/main/java/com/acasloa946/blackjack/userInterface/1vs1ViewModel.kt
@@ -1,10 +1,12 @@
 package com.acasloa946.blackjack.userInterface
+
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.acasloa946.blackjack.data.Baraja
 import com.acasloa946.blackjack.data.Carta
+import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_OFF
 
 class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
 
@@ -28,8 +30,10 @@ class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
     private val _estadoJ2 = MutableLiveData<Boolean>()
 
     private val _J1HaTerminado = MutableLiveData<Boolean>()
+    val J1HaTerminado: LiveData<Boolean> = _J1HaTerminado
 
     private val _J2HaTerminado = MutableLiveData<Boolean>()
+    val J2HaTerminado: LiveData<Boolean> = _J2HaTerminado
 
     private val _J1HaPasado = MutableLiveData<Boolean>()
 
@@ -41,6 +45,8 @@ class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
     private var _valorJ2 = MutableLiveData<Int>()
     val valorJ2: LiveData<Int> = _valorJ2
 
+    private var _mensajeFinal = MutableLiveData<String>()
+    val mensajeFinal: LiveData<String> = _mensajeFinal
 
 
 
@@ -53,6 +59,8 @@ class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
         _valorJ2.value = 0
         _J1HaTerminado.value = false
         _J2HaTerminado.value = false
+        _J1HaPasado.value = false
+        _J2HaPasado.value = false
         controlTurno()
         _refreshTxtTurno.value = false
     }
@@ -68,12 +76,12 @@ class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
         _refreshPlayerCards.value = false
         refrescarCartas()
         calcularTxtValor()
-
     }
 
     fun refrescarCartas() {
         _refreshPlayerCards.value = !_refreshPlayerCards.value!!
     }
+
     fun refrescarTxtTurno() {
         _refreshTxtTurno.value = !_refreshTxtTurno.value!!
     }
@@ -83,7 +91,7 @@ class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
         if (valorJ1.value!! > 21 || _J1HaPasado.value == true) {
             _J1HaTerminado.value = true
         }
-        if (valorJ2.value!! > 21|| _J2HaPasado.value == true) {
+        if (valorJ2.value!! > 21 || _J2HaPasado.value == true) {
             _J2HaTerminado.value = true
         }
         if (_J1HaTerminado.value == true) {
@@ -127,33 +135,81 @@ class ViewModel1vs1(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    fun controlTxtTurno():String {
-        if (_estadoJ1.value==true && _estadoJ2.value==false) {
-
+    fun controlTxtTurno(): String {
+        if (_estadoJ1.value == true && _estadoJ2.value == false) {
             return "Turno -> Jugador 1"
-        }
-        else if (_estadoJ2.value==true && _estadoJ1.value==false) {
+        } else if (_estadoJ2.value == true && _estadoJ1.value == false) {
             return "Turno -> Jugador 2"
         }
         return "Partida finalizada"
     }
 
     fun jugadorPasa() {
-        if (_estadoJ1.value == true && _J2HaTerminado.value == false) {
+        if (_estadoJ1.value == true && (_J2HaTerminado.value == false || _J2HaPasado.value==true)) {
             _J1HaPasado.value = true
             _estadoJ2.value = true
             _estadoJ1.value = false
 
-        }
-        else if (_estadoJ2.value == true && _J1HaTerminado.value == false) {
+        } else if (_estadoJ2.value == true && (_J1HaTerminado.value == false || _J1HaPasado.value==true)) {
             _J2HaPasado.value = true
             _estadoJ1.value = true
+            _estadoJ2.value = false
+        }
+        else if (_estadoJ1.value == true && _J2HaTerminado.value == true) {
+            _J1HaPasado.value = true
+            _estadoJ1.value = false
+            _estadoJ2.value = false
+        }
+        else if (_estadoJ2.value == true && _J1HaTerminado.value == true) {
+            _J2HaPasado.value = true
+            _estadoJ1.value = false
             _estadoJ2.value = false
         }
         refrescarTxtTurno()
     }
 
+    fun reiniciar() {
+        _estadoJ1.value = true
+        _estadoJ2.value = false
+        baraja.borrarBaraja()
+        controlTurno()
+        baraja.crearBaraja()
+        baraja.barajar()
+        _handP1.value = mutableListOf()
+        _handP2.value = mutableListOf()
+        _J1HaPasado.value = false
+        _J2HaPasado.value = false
+        _valorJ1.value = 0
+        _valorJ2.value = 0
+        _J1HaTerminado.value = false
+        _J2HaTerminado.value = false
+        _refreshTxtTurno.value = false
+    }
 
+    fun msjToastFinal() {
+        if (_J1HaTerminado.value == true && _J2HaTerminado.value == true) {
+            if (_J1HaPasado.value==true && _J2HaPasado.value==true) {
+                if (_valorJ1.value!! > _valorJ2.value!!) {
+                    _mensajeFinal.value = "Ha ganado el jugador 1"
+                }
+                else if (_valorJ1.value!! < _valorJ2.value!!) {
+                    _mensajeFinal.value = "Ha ganado el jugador 2"
+                }
+                else if (_valorJ1.value!! == _valorJ2.value!!) {
+                    _mensajeFinal.value = "Empate."
+                }
+            }
+            else if (_J1HaPasado.value==true && _J2HaPasado.value==false) {
+                _mensajeFinal.value = "Ha ganado el jugador 1."
+            }
+            else if (_J1HaPasado.value==false && _J2HaPasado.value==true) {
+                _mensajeFinal.value = "Ha ganado el jugador 2."
+            }
+            else if (_J1HaPasado.value==false && _J2HaPasado.value==false) {
+                _mensajeFinal.value = "La partida ha finalizado. No ha  ganado nadie."
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/acasloa946/blackjack/userInterface/Screen1vs1.kt
+++ app/src/main/java/com/acasloa946/blackjack/userInterface/Screen1vs1.kt
@@ -1,34 +1,46 @@
 package com.acasloa946.blackjack.userInterface
 
 import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.acasloa946.blackjack.data.Carta
 import com.acasloa946.blackjack.R
@@ -41,6 +53,11 @@ fun Pantalla1vs1(NavController: NavController, viewModel1vs1: ViewModel1vs1) {
     val handP2: List<Carta> by viewModel1vs1.handP2.observeAsState(listOf())
     val refreshPlayerCards: Boolean by viewModel1vs1.refreshPlayerCards.observeAsState(initial = false)
     val refreshTxtTurno: Boolean by viewModel1vs1.refreshTxtTurno.observeAsState(initial = false)
+    var showDialog: Boolean by rememberSaveable { mutableStateOf(false) }
+    var ocultarCartas : Boolean by rememberSaveable { mutableStateOf(false) }
+    val J1HaTerminado: Boolean by viewModel1vs1.J1HaTerminado.observeAsState(initial = false)
+    val J2HaTerminado: Boolean by viewModel1vs1.J2HaTerminado.observeAsState(initial = false)
+
 
 
 
@@ -55,29 +72,27 @@ fun Pantalla1vs1(NavController: NavController, viewModel1vs1: ViewModel1vs1) {
     ) {
         viewModel1vs1.controlTurno()
         viewModel1vs1.controlTxtTurno()
-        TextoTurno(viewModel1vs1,refreshTxtTurno)
+        TextoTurno(viewModel1vs1, refreshTxtTurno)
         RowBotonesJugadores(viewModel1vs1)
         Spacer(modifier = Modifier.padding(bottom = 15.dp))
-        Text(
-            text = "Jugador 1",
-            fontSize = 25.sp,
-            fontFamily = FontFamily.Monospace,
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 30.dp),
-            fontStyle = FontStyle.Italic
-        )
+        TextoJugador(player = 1, handP1, handP2)
         ManoJugador1(handP1, refreshPlayerCards)
-        TextoValor(viewModel1vs1 = viewModel1vs1, player = 1)
-        Text(
-            text = "Jugador 2",
-            fontSize = 25.sp,
-            fontFamily = FontFamily.Monospace,
-            color = Color.Black,
-            modifier = Modifier.padding(top = 30.dp, bottom = 30.dp),
-            fontStyle = FontStyle.Italic
-        )
-        ManoJugador2(handP2 = handP2, refreshPlayerCards = refreshPlayerCards)
-        TextoValor(viewModel1vs1 = viewModel1vs1, player = 2)
+        TextoValor(viewModel1vs1 = viewModel1vs1, player = 1, handP1, handP2,ocultarCartas,J1HaTerminado, J2HaTerminado)
+        TextoJugador(2, handP1, handP2)
+        ManoJugador2(handP2 = handP2, refreshPlayerCards = refreshPlayerCards,ocultarCartas, J1HaTerminado, J2HaTerminado)
+        TextoValor(viewModel1vs1 = viewModel1vs1, player = 2, handP1, handP2,ocultarCartas,J1HaTerminado, J2HaTerminado)
+        BotonReiniciar(viewModel1vs1 = viewModel1vs1,J1HaTerminado, J2HaTerminado)
+        BotonOpciones(handP1 = handP1, handP2 = handP2, cambiarShowDialog = {
+            showDialog = !showDialog
+        })
+        DialogOpciones(showDialog = showDialog, cambiarShowDialog = {
+            showDialog = !showDialog
+        },
+            ocultarCartas,
+            cambiarOcultarCartas = {
+                ocultarCartas = it
+            })
+
 
     }
 }
@@ -85,9 +100,9 @@ fun Pantalla1vs1(NavController: NavController, viewModel1vs1: ViewModel1vs1) {
 
 @Composable
 fun ManoJugador1(handP1: List<Carta>, refreshPlayerCards: Boolean) {
-    LazyRow (
+    LazyRow(
         horizontalArrangement = Arrangement.spacedBy(-100.dp)
-    ){
+    ) {
         items(handP1) {
             CrearImagen(foto = it.IdDrawable)
         }
@@ -95,15 +110,28 @@ fun ManoJugador1(handP1: List<Carta>, refreshPlayerCards: Boolean) {
 }
 
 @Composable
-fun ManoJugador2(handP2: List<Carta>, refreshPlayerCards: Boolean) {
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(-100.dp)
-    ) {
-        items(handP2) {
-            CrearImagen(foto = it.IdDrawable)
+fun ManoJugador2(handP2: List<Carta>, refreshPlayerCards: Boolean, ocultarCartas: Boolean,J1HaTerminado:Boolean,J2HaTerminado:Boolean) {
+    if (!ocultarCartas || (J1HaTerminado && J2HaTerminado)) {
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(-100.dp)
+        ) {
+            items(handP2) {
+                CrearImagen(foto = it.IdDrawable)
+            }
         }
     }
+    else if (ocultarCartas) {
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(-100.dp)
+        ) {
+            items(handP2) {
+                CrearImagen(foto = R.drawable.facedown)
+            }
+        }
+    }
+
 }
+
 @Composable
 fun CrearImagen(foto: Int) {
     Image(
@@ -116,7 +144,6 @@ fun CrearImagen(foto: Int) {
 @Composable
 private fun RowBotonesJugadores(viewModel1vs1: ViewModel1vs1) {
     Row(
-        Modifier.padding(bottom = 10.dp)
     ) {
         Button(
             shape = RectangleShape,
@@ -142,35 +169,48 @@ private fun RowBotonesJugadores(viewModel1vs1: ViewModel1vs1) {
         }
     }
 }
+
 @Composable
-fun TextoValor(viewModel1vs1:ViewModel1vs1,player:Int) {
+fun TextoValor(
+    viewModel1vs1: ViewModel1vs1,
+    player: Int,
+    handP1: List<Carta>,
+    handP2: List<Carta>,
+    ocultarCartas: Boolean,
+    J1HaTerminado: Boolean,
+    J2HaTerminado: Boolean
+) {
     val valorJ1: Int by viewModel1vs1.valorJ1.observeAsState(initial = 0)
     val valorJ2: Int by viewModel1vs1.valorJ2.observeAsState(initial = 0)
 
-    if (player == 1) {
-        Text(
-            text = "Valor: $valorJ1",
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Monospace,
-            color = Color.Black,
-            modifier = Modifier.padding(top = 15.dp, bottom = 15.dp),
-            fontStyle = FontStyle.Italic
-        )
-    }
-    else {
-        Text(
-            text = "Valor: $valorJ2",
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Monospace,
-            color = Color.Black,
-            modifier = Modifier.padding(top = 15.dp, bottom = 15.dp),
-            fontStyle = FontStyle.Italic
-        )
-    }
+    if (handP1.isNotEmpty() || handP2.isNotEmpty()) {
+        if (player == 1) {
+            Text(
+                text = "Valor: $valorJ1",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
+                fontStyle = FontStyle.Italic
+            )
+        } else {
+            if (!ocultarCartas || (J1HaTerminado && J2HaTerminado)) {
+                Text(
+                    text = "Valor: $valorJ2",
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Monospace,
+                    color = Color.Black,
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
+                    fontStyle = FontStyle.Italic
+                )
+            }
+        }
 
+    }
 }
+
 @Composable
-fun TextoTurno(viewModel1vs1: ViewModel1vs1,refreshTxtTurno:Boolean) {
+fun TextoTurno(viewModel1vs1: ViewModel1vs1, refreshTxtTurno: Boolean) {
     Text(
         text = viewModel1vs1.controlTxtTurno(),
         fontSize = 20.sp,
@@ -181,6 +221,101 @@ fun TextoTurno(viewModel1vs1: ViewModel1vs1,refreshTxtTurno:Boolean) {
     )
 }
 
+@Composable
+fun BotonReiniciar(viewModel1vs1: ViewModel1vs1,J1HaTerminado: Boolean,J2HaTerminado: Boolean) {
+
+    var mostrar = true
+    val mensajeFinal: String by viewModel1vs1.mensajeFinal.observeAsState(initial = "")
+
+
+    if (J1HaTerminado && J2HaTerminado && mostrar) {
+        viewModel1vs1.msjToastFinal()
+        TextoFinal(mensajeFinal)
+        mostrar = false
+        Button(
+            shape = RectangleShape,
+            onClick = { viewModel1vs1.reiniciar() },
+            colors = ButtonDefaults.buttonColors(Color.Red)
+        ) {
+            Text(
+                text = "Reiniciar", textAlign = TextAlign.Center, color = Color.Black
+            )
+        }
+    }
+}
+
+@Composable
+fun TextoJugador(player: Int, handP1: List<Carta>, handP2: List<Carta>) {
+    if (handP1.isNotEmpty() || handP2.isNotEmpty()) {
+        Text(
+            text = "Jugador $player",
+            fontSize = 25.sp,
+            fontFamily = FontFamily.Monospace,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 15.dp, bottom = 15.dp),
+            fontStyle = FontStyle.Italic
+        )
+    }
+}
+
+@Composable
+fun TextoFinal(string: String) {
+    val context = LocalContext.current
+    Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun BotonOpciones(handP1: List<Carta>, handP2: List<Carta>, cambiarShowDialog: () -> Unit) {
+    if (handP1.isEmpty() && handP2.isEmpty()) {
+        Button(
+            shape = RectangleShape,
+            onClick = { cambiarShowDialog() },
+            colors = ButtonDefaults.buttonColors(Color.Red)
+        ) {
+            Text(text = "Opciones", textAlign = TextAlign.Center, color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun DialogOpciones(showDialog: Boolean, cambiarShowDialog: () -> Unit, ocultarCartas : Boolean, cambiarOcultarCartas : (Boolean) -> Unit) {
+    if (showDialog) {
+        Dialog(onDismissRequest = { cambiarShowDialog() }) {
+            Column(
+                modifier = Modifier
+                    .background(Color.Black)
+                    .padding(top = 10.dp, bottom = 10.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Esconder cartas",
+                    color = Color.White
+                )
+                Checkbox(
+                    checked = ocultarCartas, onCheckedChange = {
+                        cambiarOcultarCartas(it)
+                    },
+                    colors = CheckboxDefaults.colors(Color.Red)
+                )
+                Button(
+                    shape = RectangleShape,
+                    onClick = {
+                        cambiarShowDialog()
+                    },
+                    colors = ButtonDefaults.buttonColors(Color.Red)
+                ) {
+                    Text(
+                        text = "Guardar y salir", textAlign = TextAlign.Center, color = Color.White
+                    )
+
+                }
+            }
+        }
+    }
+}
+    
 
 
 
